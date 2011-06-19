9eec92b3dd73616d76118145d135634bc20b4042
Janice Collier <ardaniel.sumhenner@gmail.com>
2011-06-18 17:28:58 -0700

    adding room code; still WIP

--- ex43-code.py
+++ ex43-code.py
@@ -2,6 +2,8 @@ from random import choice
 
 class MainHub(object):
 
+# Eventually I'd like the codeword to be randomly selected; not there yet
+
 	def room_setup(self):
 		room_desc = """
 This is the main hub of the space station. There is a 
@@ -10,9 +12,9 @@ and give the Chief Diplomat the codeword 'cookie.'"
 """
 		print room_desc
 		
-		prompt = raw_input("> ")
+		move = raw_input("> ")
 		
-		if prompt == "go north":
+		if "north" in move and "do not" not in move:
 			print "The guide smiles and you head north."
 			return 'DiplomatRoom'
 
@@ -48,6 +50,7 @@ leafiest alien you've ever seen.  It extends a tendril towards you inquiringly."
 		if "give" in offer_item and "envelope" in offer_item and "do not" not in offer_item:
 			print "You think the alien looks pleased; it takes the envelope and"
 			print "promptly digests it.  One of its vines points you back to the diplomat's office."
+			print "(You think you hear a noise in there...)"
 			return 'DiplomatOffice2'
 			
 		else:
@@ -55,5 +58,38 @@ leafiest alien you've ever seen.  It extends a tendril towards you inquiringly."
 			print "You might be pooped out in 500 years."
 			return 'death'
 
-a = AlienRoom()
-a.room_setup()
+class DiplomatOffice2(object):
+	def room_setup(self):
+		room_desc="""Back in the diplomat's office, you find that she's been replaced with a robot.
+It levels a blaster at you and fires."""
+
+		print room_desc
+		evade = raw_input("> ")
+		
+		if "dodge" or "move" in evade:
+			print "You dodge expertly.  You spot a shiny plaque on the wall near your head."
+			print "What are you going to do now? The robot prepares to fire again."
+			
+			grab = raw_input("> ")
+			
+			if "plaque" in grab:
+				print "You hold the plaque in front of you as the robot fires."
+				print "The blast strikes the plaque, bounces off, and hits the robot."
+				print "The robot explodes, leaving a chip behind. You pocket it and go"
+				print "back to the main hub.:"
+				return 'MainHub2'
+			
+			else:
+				print "The robot shoots you dead.  Station security finds"
+				print "your corpse and assumes you killed the diplomat,"
+				print "making you a posthumous assassin.  Way to go, pal."
+				return 'death'
+
+		else:
+			print "You fail to dodge. The robot blows a hole in your head."
+			print "Everyone assumes you killed the diplomat. Alien relations"
+			print "are set back a billion years because you didn't move. You suck."
+			return 'death'
+
+d2 = DiplomatOffice2()
+d2.room_setup()
