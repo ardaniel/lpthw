220dd44d16fe9be21979a1385f66994b18eae6c8
Janice Collier <ardaniel.sumhenner@gmail.com>
2011-06-19 18:39:14 -0700

    moved locations dict out of play() and into the class proper

--- ex43engine.py
+++ ex43engine.py
@@ -1,28 +1,28 @@
 import ex43rooms as rooms
 
 class Engine(object):
-		
+
+	LOCATIONS = {
+	'Death': rooms.Death(),
+	'MainHub': rooms.MainHub(),
+	'MainHub2': rooms.MainHub2(),
+	'DiplomatRoom': rooms.DiplomatRoom(),
+	'DiplomatRoom2': rooms.DiplomatRoom2(),
+	'AlienRoom': rooms.AlienRoom(),
+	'SideDoor': rooms.SideDoor(),
+	'Victory': rooms.Victory(),
+	'Death': rooms.Death()
+	}	
+	
 	def __init__(self, start):
 		self.start = start
 			
 	def play(self):
-	
-		LOCATIONS = {
-		'Death': rooms.Death(),
-		'MainHub': rooms.MainHub(),
-		'MainHub2': rooms.MainHub2(),
-		'DiplomatRoom': rooms.DiplomatRoom(),
-		'DiplomatRoom2': rooms.DiplomatRoom2(),
-		'AlienRoom': rooms.AlienRoom(),
-		'SideDoor': rooms.SideDoor(),
-		'Victory': rooms.Victory(),
-		'Death': rooms.Death()
-		}	
 		
 		next = self.start.room_setup()
 
 		while True:
-			next = LOCATIONS[next].room_setup()
+			next = self.LOCATIONS[next].room_setup()
 				
 			
 the_game = Engine(rooms.MainHub())
